6494268ec9383c351b199c4bbc5755f792384830
Kevin <kevingabriel18@gamil.com>
2025-10-20 16:51:26 -0400

    agregar campos de formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,55 @@ import React from 'react';
 
 const Form = () =>{
 return (
-  <h1>Desde El Formulario</h1>
+  <>
+  <h1>Crear Cita</h1>
+  <form>
+    
+    <label>Nombre de Mascota</label>
+    <input
+      type="text"
+      name="pet_owner"
+      className="u-full-width"
+      placeholder="Nombre Dueño de Mascota"
+    />
+    
+    <label>Fecha</label>
+    <input
+      type="date"
+      name="date"
+      className="u-full-width"
+    />
+    
+    
+    <label>Hora</label>
+    <input
+      type="date"
+      name="time"
+      className="u-full-width"
+    />
+    
+    <label>Nombre de Mascota</label>
+    <input
+      type="text"
+      name="mascot"
+      className="u-full-width"
+      placeholder="Nombre de Mascota"
+    />
+    
+    <label>Sintomas</label>
+    <textarea
+      className="u-full-width"
+      name="symptoms"
+    ></textarea>
+    
+    <button
+      type="submit"
+      name="u-full-width button-primary"
+    >
+      Agregar Cita
+    </button>
+  </form>
+  </>
   );
 }
 
